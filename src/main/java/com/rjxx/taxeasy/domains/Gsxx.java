67202cbf425b00044d4010b8dbb4ddc60a73646b
liuyuanya <524684541@qq.com>
2017-01-13 17:27:32 +0800

    修改bug

--- src/main/java/com/rjxx/taxeasy/domains/Gsxx.java
+++ src/main/java/com/rjxx/taxeasy/domains/Gsxx.java
@@ -6,6 +6,9 @@ import com.rjxx.comm.json.JsonDateFormat;
 import com.rjxx.comm.json.JsonDatetimeFormat;
 import org.springframework.format.annotation.DateTimeFormat;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 /**
  * t_gsxx 实体类
@@ -62,6 +65,22 @@ public class Gsxx  implements Serializable {
  */ 
 @Column(name="wxsecret")
 	protected String wxsecret;
+@Column(name="xfnum")
+protected Integer xfnum;
+@Column(name="kpdnum")
+protected Integer kpdnum;
+@Column(name="yhnum")
+protected Integer yhnum;
+@Column(name="kpnum")
+protected Integer kpnum;
+@Column(name="yxqsrq")
+@JsonSerialize(using = JsonDatetimeFormat.class)
+@DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+	protected Date yxqsrq;
+@Column(name="yxjzrq")
+@JsonSerialize(using = JsonDatetimeFormat.class)
+@DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+	protected Date yxjzrq;
 
 	public String getAppKey() {
 	return appKey;
@@ -127,6 +146,70 @@ public void setWsUrl(String wsUrl) {
 
 	public void setWxsecret(String wxsecret) {
 		this.wxsecret = wxsecret;
+	}
+
+	public Integer getXfnum() {
+		return xfnum;
+	}
+
+	public void setXfnum(Integer xfnum) {
+		this.xfnum = xfnum;
+	}
+
+	public Integer getKpdnum() {
+		return kpdnum;
+	}
+
+	public void setKpdnum(Integer kpdnum) {
+		this.kpdnum = kpdnum;
+	}
+
+	public Integer getYhnum() {
+		return yhnum;
+	}
+
+	public void setYhnum(Integer yhnum) {
+		this.yhnum = yhnum;
+	}
+
+	public Integer getKpnum() {
+		return kpnum;
+	}
+
+	public void setKpnum(Integer kpnum) {
+		this.kpnum = kpnum;
+	}
+
+	public Date getYxqsrq() {
+		return yxqsrq;
+	}
+	
+	public String getQsrq(){
+		if (yxqsrq != null) {
+			return new SimpleDateFormat("yyyy年MM月dd日").format(yxqsrq);
+		}
+		
+		return null;
+	}
+
+	public void setYxqsrq(Date yxqsrq) {
+		this.yxqsrq = yxqsrq;
+	}
+
+	public Date getYxjzrq() {
+		return yxjzrq;
+	}
+	
+	public String getJzrq(){
+		if (yxjzrq != null) {
+			return new SimpleDateFormat("yyyy年MM月dd日").format(yxjzrq);
+		}
+		
+		return null;
+	}
+
+	public void setYxjzrq(Date yxjzrq) {
+		this.yxjzrq = yxjzrq;
 	}
 
 }
