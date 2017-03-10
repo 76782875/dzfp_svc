89bcaf9461c5819963d15d259ebd5aacadb2d67a
liuyuanya <524684541@qq.com>
2017-03-10 16:34:06 +0800

    修改发票发送

--- src/main/java/com/rjxx/taxeasy/vo/KplsVO.java
+++ src/main/java/com/rjxx/taxeasy/vo/KplsVO.java
@@ -19,6 +19,8 @@ public class KplsVO {
 
     protected String gfmc;
 
+    protected String xfmc;
+
     protected String jylssj;
 
     protected String fpdm;
@@ -298,5 +300,13 @@ public class KplsVO {
 
 	public void setFpzlmc(String fpzlmc) {
 		this.fpzlmc = fpzlmc;
+	}
+
+	public String getXfmc() {
+		return xfmc;
+	}
+
+	public void setXfmc(String xfmc) {
+		this.xfmc = xfmc;
 	}
 }
