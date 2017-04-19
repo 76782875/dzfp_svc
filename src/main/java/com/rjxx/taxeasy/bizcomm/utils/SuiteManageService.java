00999e8b4764e33efd50679a085460ea2c91984d
许黎明 <1084854604@qq.com>
2017-04-19 18:34:03 +0800

    钉钉

---
+++ src/main/java/com/rjxx/taxeasy/bizcomm/utils/SuiteManageService.java
@@ -0,0 +1,105 @@
+package com.rjxx.taxeasy.bizcomm.utils;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSON;
+import com.dingtalk.open.client.ServiceFactory;
+import com.dingtalk.open.client.api.model.isv.SuiteToken;
+import com.dingtalk.open.client.api.service.isv.IsvService;
+import com.rjxx.taxeasy.dingding.Helper.ConfOapiRequestHelper;
+import com.rjxx.taxeasy.domains.IsvCorpSuiteJsapiTicket;
+import com.rjxx.taxeasy.domains.IsvCorpToken;
+import com.rjxx.taxeasy.domains.IsvSuite;
+import com.rjxx.taxeasy.domains.IsvSuiteTicket;
+import com.rjxx.taxeasy.domains.IsvSuiteToken;
+import com.rjxx.taxeasy.service.IsvCorpSuiteJsapiTicketService;
+import com.rjxx.taxeasy.service.IsvCorpTokenService;
+import com.rjxx.taxeasy.service.IsvSuiteService;
+import com.rjxx.taxeasy.service.IsvSuiteTicketService;
+import com.rjxx.taxeasy.service.IsvSuiteTokenService;
+/**
+ * 定时任务更新token
+ * @author xlm
+ *
+ */
+@Service
+public class SuiteManageService {
+    private static Logger logger = LoggerFactory.getLogger(SuiteManageService.class);
+
+	@Autowired
+    private IsvSuiteService  isvsuiteservice;
+	
+	@Autowired
+    private IsvSuiteTicketService  isvsuiteticketservice;
+	
+	
+	@Autowired
+    private IsvSuiteTokenService isvsuitetokenservice;
+	@Autowired
+	private IsvCorpSuiteJsapiTicketService isvcorpsuitejsapiticketservice;
+	@Autowired
+	private IsvCorpTokenService isvcorptokenservice;
+	@Autowired
+    private ConfOapiRequestHelper confOapiRequestHelper;
+	
+	public boolean saveOrUpdateSuiteToken(String suiteKey) {
+		// TODO Auto-generated method stub
+        try {
+        	Map params=new HashMap();
+    		params.put("suiteKey", suiteKey);
+    		IsvSuite suiteBO = isvsuiteservice.getIsvSuite(params);
+            IsvSuiteTicket suiteTicketDO = isvsuiteticketservice.findOneByParams(params);
+            //调用钉钉接口
+            ServiceFactory serviceFactory = ServiceFactory.getInstance();
+    		IsvService isvService = serviceFactory.getOpenService(IsvService.class);
+			SuiteToken suiteToken = isvService.getSuiteToken(suiteBO.getSuiteKey(), suiteBO.getSuiteSecret(), suiteTicketDO.getTicket());
+			IsvSuiteToken suiteTokenDO=new IsvSuiteToken();
+	            suiteTokenDO.setGmtCreate(new Date());
+	            suiteTokenDO.setGmtModified(new Date());
+	            Calendar calendar = Calendar.getInstance();
+	            calendar.setTime(new Date());
+	            calendar.add(Calendar.SECOND,(int)suiteToken.getExpires_in());
+	            suiteTokenDO.setExpiredTime(calendar.getTime());
+	            suiteTokenDO.setSuiteKey(suiteKey);
+	            suiteTokenDO.setSuiteToken(suiteToken.getSuite_access_token());
+	            isvsuitetokenservice.save(suiteTokenDO);
+	    		return true;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+    		return false;
+		}
+	}
+
+	public boolean getCorpJSAPITicket(String suiteKey, String corpId) {
+		// TODO Auto-generated method stub
+		try{
+				Map params=new HashMap<>();
+				params.put("suiteKey", suiteKey);
+				params.put("corpId", corpId);
+		        IsvCorpSuiteJsapiTicket corpJSTicketDO = isvcorpsuitejsapiticketservice.findOneByParams(params);
+		        Calendar calendar = Calendar.getInstance();
+		        calendar.setTime(new Date());
+		        calendar.add(Calendar.MINUTE, 10);//为了防止误差,提前10分钟更新jsticket
+		        if (null == corpJSTicketDO || calendar.getTime().compareTo(corpJSTicketDO.getExpiredTime()) != -1) {
+		            IsvCorpToken corpTokenVoSr = isvcorptokenservice.findOneByParams(params);
+		            IsvCorpSuiteJsapiTicket jsAPITicketSr = confOapiRequestHelper.getJSTicket(suiteKey, corpId, corpTokenVoSr.getCorpToken());
+		        	logger.info("jsapiticket:{}"+JSON.toJSONString(jsAPITicketSr));
+		        	corpJSTicketDO=jsAPITicketSr;
+		        }
+		        isvcorpsuitejsapiticketservice.save(corpJSTicketDO);
+				return true;
+
+		}catch(Exception e){
+			return false;
+		}
+	}
+}
