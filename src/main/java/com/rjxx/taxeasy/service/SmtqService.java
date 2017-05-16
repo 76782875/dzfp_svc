8efe08de566a4633fc111a541db9c561250e0194
张兵 <zhangbing@datarj.com>
2017-05-16 09:06:29 +0800

    扫描提取同一事务保存t_jyls和t_jyspmx

--- src/main/java/com/rjxx/taxeasy/service/SmtqService.java
+++ src/main/java/com/rjxx/taxeasy/service/SmtqService.java
@@ -3,9 +3,12 @@ package com.rjxx.taxeasy.service;
 import com.rjxx.comm.mybatis.Pagination;
 import com.rjxx.taxeasy.dao.SmtqJpaDao;
 import com.rjxx.taxeasy.dao.SmtqMapper;
+import com.rjxx.taxeasy.domains.Jyls;
+import com.rjxx.taxeasy.domains.Jyspmx;
 import com.rjxx.taxeasy.domains.Smtq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
@@ -16,7 +19,7 @@ import java.util.Map;
  * Mon Nov 07 16:41:00 CST 2016
  *
  * @WangYong
- */ 
+ */
 @Service
 public class SmtqService {
 
@@ -25,6 +28,12 @@ public class SmtqService {
 
     @Autowired
     private SmtqMapper smtqMapper;
+
+    @Autowired
+    private JylsService jylsService;
+
+    @Autowired
+    private JyspmxService jyspmxService;
 
     public Smtq findOne(int id) {
         return smtqJpaDao.findOne(id);
@@ -49,9 +58,17 @@ public class SmtqService {
     public List<Smtq> findByPage(Pagination pagination) {
         return smtqMapper.findByPage(pagination);
     }
-    
-    public List<Smtq> findAll(Map params){
-    	return smtqMapper.findAll(params);
+
+    public List<Smtq> findAll(Map params) {
+        return smtqMapper.findAll(params);
+    }
+
+    @Transactional
+    public void saveAll(Jyls jyls, Jyspmx jyspmx, Smtq smtq) {
+        jylsService.save(jyls);
+        jyspmx.setDjh(jyls.getDjh());
+        jyspmxService.save(jyspmx);
+        this.save(smtq);
     }
 
 }
