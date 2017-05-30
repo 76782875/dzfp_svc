d9a8a387b74667f44dffb2116e2885282d3f45ba
许黎明 <1084854604@qq.com>
2017-05-30 17:04:17 +0800

    发票作废

--- src/main/java/com/rjxx/taxeasy/bizcomm/utils/FpzfService.java
+++ src/main/java/com/rjxx/taxeasy/bizcomm/utils/FpzfService.java
@@ -1,37 +1,15 @@
 package com.rjxx.taxeasy.bizcomm.utils;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.rjxx.taxeasy.domains.Cszb;
-import com.rjxx.taxeasy.domains.Jyls;
 import com.rjxx.taxeasy.domains.Kpls;
-import com.rjxx.taxeasy.service.CszbService;
-import com.rjxx.taxeasy.service.FpgzService;
-import com.rjxx.taxeasy.service.GsxxService;
-import com.rjxx.taxeasy.service.JylsService;
-import com.rjxx.taxeasy.service.JyspmxService;
 import com.rjxx.taxeasy.service.KplsService;
-import com.rjxx.taxeasy.service.KpspmxService;
-import com.rjxx.taxeasy.service.XfService;
 
 @Service
 public class FpzfService {
 	
 	 @Autowired private KplsService kplsService;
-	 @Autowired private JylsService jylsService;
-	 @Autowired private FpgzService fpgzService;
-	 @Autowired private SkService skService;
-	 @Autowired private JyspmxService jymxService;
-	 @Autowired private GsxxService gsxxService;
-	 @Autowired private KpspmxService kpspmxService;
-	 @Autowired private DataOperte dc;
-	 @Autowired private XfService xfService;
-	 @Autowired private CszbService cszbService;
+
 	 
 	//作废处理
 		public InvoiceResponse zfcl(Integer kplsh,Integer yhid,String gsdm) throws Exception {
