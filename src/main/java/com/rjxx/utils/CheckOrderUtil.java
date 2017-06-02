43c2b41b188249ec9e390f4abdeccc0ae145325d
张兵 <zhangbing@datarj.com>
2017-06-02 13:18:52 +0800

    修改email的校验

--- src/main/java/com/rjxx/utils/CheckOrderUtil.java
+++ src/main/java/com/rjxx/utils/CheckOrderUtil.java
@@ -135,7 +135,7 @@ public class CheckOrderUtil {
             // email
             String Email = (String) jyxxsq.getGfemail();
             if (Email != null && !Email.equals("") && !Email
-                    .matches("^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$")) {
+                    .matches("^(\\w)+(\\.\\w+)*@(\\w)+((\\.\\w+)+)$")) {
                 result += ddh + ":请求参数<Email>格式有误;";
             }
             // 提取码校验
