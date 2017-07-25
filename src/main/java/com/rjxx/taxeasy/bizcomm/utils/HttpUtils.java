08cd7c5b9cb236accf03225f8edef01c75f44145
许黎明 <1084854604@qq.com>
2017-07-25 19:12:46 +0800

    修改接口调用地址

--- src/main/java/com/rjxx/taxeasy/bizcomm/utils/HttpUtils.java
+++ src/main/java/com/rjxx/taxeasy/bizcomm/utils/HttpUtils.java
@@ -2,6 +2,9 @@ package com.rjxx.taxeasy.bizcomm.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rjxx.utils.SignUtils;
+import com.rjxx.utils.XmlJaxbUtils;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +23,10 @@ public class HttpUtils {
 
     private  static Logger logger = LoggerFactory.getLogger(HttpUtils.class);
 
+
+    //public static String WS_URL = "http://open.datarj.com/webService/services/invoiceService?wsdl";
+    public static String WS_URL = "http://test.datarj.com/webService/services/invoiceService?wsdl";
+    //public static String WS_URL = "http://localhost:8080/services/invoiceService?wsdl";
     /**
      * 调用2.0接口开票
      * @param QueryData
@@ -28,47 +35,19 @@ public class HttpUtils {
      * @return
      */
     public static String HttpUrlPost(String QueryData,String AppId,String key){
-        String sign= SignUtils.getSign(QueryData,key);
-        ObjectMapper mapper=new ObjectMapper();
-        Map param = new HashMap();
-        param.put("methodName", "UploadOrderData");
-        param.put("AppKey",AppId);
-        param.put("Secret", sign);
-        param.put("Operation","01");
-        param.put("InvoiceData",QueryData);
-        BufferedReader reader = null;
-        StringBuffer buffer = new StringBuffer();
+        String result="";
         try {
-            String jsonString = mapper.writeValueAsString(param);
-            String strMessage = "";
-            // 接报文的地址
-            String urldev="http://localhost:8080/dzfp-service-new/service";//本地
-            String urldev1="http://test.datarj.com/webService/service";//测试库
-            String url="http://open2.datarj.com/webService/service";//生产库
-            URL uploadServlet = new URL(urldev1);
-            HttpURLConnection servletConnection = (HttpURLConnection) uploadServlet.openConnection();
-            // 设置连接参数
-            servletConnection.setRequestProperty("Content-Type", "plain/text; charset=UTF-8");
-            servletConnection.setRequestMethod("POST");
-            servletConnection.setDoOutput(true);
-            servletConnection.setDoInput(true);
-            servletConnection.setAllowUserInteraction(true);
-            // 开启流，写入XML数据
-            OutputStream output = servletConnection.getOutputStream();
-            logger.info("----------发送的报文------"+jsonString.toString());
-            output.write(jsonString.toString().getBytes());
-            output.flush();
-            output.close();
-            // 获取返回的数据
-            InputStream inputStream = servletConnection.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-            while ((strMessage = reader.readLine()) != null) {
-                buffer.append(strMessage);
-            }
-            logger.info("----------接收返回值------"+buffer.toString());
-        } catch (IOException e) {
+            JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
+            Client client = dcf.createClient(WS_URL);
+            String methodName = "UploadOrderData";
+            String sign= SignUtils.getSign(QueryData,key);
+            Object[] objects = client.invoke(methodName, AppId, sign, "01", QueryData);
+            //输出调用结果
+            result = objects[0].toString();
+            logger.info("----------接收返回值------"+result.toString());
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return buffer.toString();
+        return result;
     }
 }
