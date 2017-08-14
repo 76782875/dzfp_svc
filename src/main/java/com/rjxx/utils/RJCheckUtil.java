5926d049661fc06b04e9581b3c135f1b7b2e86f7
wangyahui <24340912@qq.com>
2017-08-14 17:01:13 +0800

    增加程序健壮性

--- src/main/java/com/rjxx/utils/RJCheckUtil.java
+++ src/main/java/com/rjxx/utils/RJCheckUtil.java
@@ -12,41 +12,52 @@ import java.util.Map;
  */
 public class RJCheckUtil {
     public static Boolean checkMD5(String key,String q){
-        Map map  =decode(q);
-        String orderNo = map.get("orderNo").toString();
-        String orderTime = map.get("orderTime").toString();
-        String price = map.get("price").toString();
-        String storeNo = map.get("storeNo").toString();
-        String sign = map.get("sign").toString();
-        String dbs = "on="+orderNo+"&ot="+orderTime+"&pr="+price+"&sn="+storeNo+"&key="+key;
-        String MD5dbs = "";
         try {
-            MD5dbs = MD5Util.generatePassword(dbs);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if(!sign.equalsIgnoreCase(MD5dbs)){
+            Map map  =decode(q);
+            if(map==null){
+                return false;
+            }
+            String orderNo = map.get("orderNo").toString();
+            String orderTime = map.get("orderTime").toString();
+            String price = map.get("price").toString();
+            String storeNo = map.get("storeNo").toString();
+            String sign = map.get("sign").toString();
+            String dbs = "on="+orderNo+"&ot="+orderTime+"&pr="+price+"&sn="+storeNo+"&key="+key;
+            String MD5dbs = "";
+            try {
+                MD5dbs = MD5Util.generatePassword(dbs);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if(!sign.equalsIgnoreCase(MD5dbs)){
+                return false;
+            }
+            return true;
+        } catch(Exception e){
             return false;
         }
-        return true;
     }
 
     public static Map decode(String q){
-        byte[] bytes = Base64.decodeBase64(q);
-        String paramsUrl = new String(bytes);
-        String[] paramsArray = paramsUrl.split("&");
-        String orderNo = paramsArray[0].substring(paramsArray[0].lastIndexOf("=") + 1);//订单号
-        String orderTime = paramsArray[1].substring(paramsArray[1].lastIndexOf("=") + 1);//订单时间
-        String price = paramsArray[2].substring(paramsArray[2].lastIndexOf("=") + 1);//价格
-        String storeNo = paramsArray[3].substring(paramsArray[3].lastIndexOf("=") + 1);//门店号
-        String sign = paramsArray[4].substring(paramsArray[4].lastIndexOf("=") + 1);//签名
-        Map<String, String> map = new HashMap<>();
-        map.put("orderNo", orderNo);
-        map.put("orderTime", orderTime);
-        map.put("price", price);
-        map.put("storeNo", storeNo);
-        map.put("sign", sign);
-        return map;
+        try {
+            byte[] bytes = Base64.decodeBase64(q);
+            String paramsUrl = new String(bytes);
+            String[] paramsArray = paramsUrl.split("&");
+            String orderNo = paramsArray[0].substring(paramsArray[0].lastIndexOf("=") + 1);//订单号
+            String orderTime = paramsArray[1].substring(paramsArray[1].lastIndexOf("=") + 1);//订单时间
+            String price = paramsArray[2].substring(paramsArray[2].lastIndexOf("=") + 1);//价格
+            String storeNo = paramsArray[3].substring(paramsArray[3].lastIndexOf("=") + 1);//门店号
+            String sign = paramsArray[4].substring(paramsArray[4].lastIndexOf("=") + 1);//签名
+            Map<String, String> map = new HashMap<>();
+            map.put("orderNo", orderNo);
+            map.put("orderTime", orderTime);
+            map.put("price", price);
+            map.put("storeNo", storeNo);
+            map.put("sign", sign);
+            return map;
+        } catch(Exception e){
+            return null;
+        }
     }
 
     public static String getQ(String key,String orderNo,String orderTime,String price,String storeNo){
@@ -70,7 +81,7 @@ public class RJCheckUtil {
         String storeNo = "sh001";
         System.out.println(getQ(key,orderNo,orderTime,price,storeNo));
     }
-//
+
 //    public static void main(String[] args) {
 //        Boolean b =RJCheckUtil.checkMD5("3f7626939b146cc47c31daf43edc42bd","b249VFNUX1JCMjAxNzA4MTQwMDAyMCZvdD0yMDE3MDgxNDEwNDUwMCZwcj01NSZzbj1zaDAwMSZzaT00NDM1ZWMwM2JkN2ZjMzA0YTA0MzhjMjVhYjU0YWFmMQ==");
 //        System.out.println(b);
