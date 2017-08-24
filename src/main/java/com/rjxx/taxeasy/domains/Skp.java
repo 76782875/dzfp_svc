31650320d6e89b9a6594503374b7039d5b2151be
许黎明 <1084854604@qq.com>
2017-08-24 15:59:54 +0800

    no message

--- src/main/java/com/rjxx/taxeasy/domains/Skp.java
+++ src/main/java/com/rjxx/taxeasy/domains/Skp.java
@@ -19,7 +19,7 @@ public class Skp implements Serializable {
 	/**
 	 * 
 	 */
-	//private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * 税控盘号
