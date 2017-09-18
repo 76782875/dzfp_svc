791284f57e406bdf25ea40f1fd945baf07a8e9ed
zhangsongqiang <zhangsongqiang@datarj.com>
2017-09-18 18:35:58 +0800

    no message

--- src/main/java/com/rjxx/taxeasy/dao/WxTokenJpaDao.java
+++ src/main/java/com/rjxx/taxeasy/dao/WxTokenJpaDao.java
@@ -3,11 +3,14 @@ package com.rjxx.taxeasy.dao;
 import com.rjxx.taxeasy.domains.WxToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Administrator on 2017-09-18.
  */
-public interface WxTokenJpaDao extends JpaRepository<WxToken,Integer>{
+@Repository
+public interface WxTokenJpaDao extends CrudRepository<WxToken,Integer> {
 
     @Query(nativeQuery = true,value = "select * from wx_token where flag=?1")
     WxToken findByFlag(String flag);
