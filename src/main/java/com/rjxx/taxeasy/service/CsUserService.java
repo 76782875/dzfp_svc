1165ea46fab23d0f02a2cc857ec4f7af88789263
wangyahui <24340912@qq.com>
2017-11-30 18:37:05 +0800

    update

--- src/main/java/com/rjxx/taxeasy/service/CsUserService.java
+++ src/main/java/com/rjxx/taxeasy/service/CsUserService.java
@@ -1,10 +1,12 @@
 package com.rjxx.taxeasy.service;
 
-import com.rjxx.taxeasy.dao.CszbMapper;
+import com.rjxx.taxeasy.dao.CszbJpaDao;
 import com.rjxx.taxeasy.dao.XfJpaDao;
 import com.rjxx.taxeasy.domains.Csb;
 import com.rjxx.taxeasy.domains.Cszb;
 import com.rjxx.taxeasy.domains.Xf;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,37 +22,29 @@ import java.util.Map;
 public class CsUserService {
 
     @Autowired
-    private CszbMapper cszbMapper;
+    private CszbJpaDao cszbJpaDao;
     @Autowired
     private CsbService csbService;
     @Autowired
     private XfJpaDao xfJpaDao;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     /**
-     *  获取有这个参数的销方,或者获取有这个参数而且参数值等于value的销方
-     *  如果获取所有有这个参数的销方，则设置value为空
-     *  如果获取参数值等于某个值得销方，则传入value
+     * 获取有这个参数而且参数值等于value的销方
      */
-    public List<Xf> getXfsByCsm(String csm,String value){
+    public List<Xf> getXfsByCsm(String csm, String value) {
+        if (value == null) {
+            logger.error("value不能为空");
+            return null;
+        }
         Map map = new HashMap<>();
         map.put("csm", csm);
         Csb csb = csbService.findOneByParams(map);
-        Map hashMap = new HashMap();
-        hashMap.put("csid", csb.getId());
-        List<Cszb> cszbs = cszbMapper.findAllByParams(hashMap);
+        List<Cszb> cszbs = cszbJpaDao.findByCsid(csb.getId());
         List<Xf> xfs = new ArrayList<>();
-        for(Cszb cszb:cszbs) {
-            if (value != null) {
-                if (value.equals(cszb.getCsz())) {
-                    Integer xfid = cszb.getXfid();
-                    if (xfid == null) {
-                        String gsdm = cszb.getGsdm();
-                        xfs.addAll(xfJpaDao.findAllByGsdm(gsdm));
-                    } else {
-                        xfs.add(xfJpaDao.findOneById(xfid));
-                    }
-                }
-            }else{
+        for (Cszb cszb : cszbs) {
+            if (value.equals(cszb.getCsz())) {
                 Integer xfid = cszb.getXfid();
                 if (xfid == null) {
                     String gsdm = cszb.getGsdm();
