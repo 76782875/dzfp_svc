aab824644b0f39345ec9ebba62e654d0a9d7a4bc
许黎明 <1084854604@qq.com>
2017-12-07 19:01:38 +0800

    no message

--- src/main/java/com/rjxx/taxeasy/bizcomm/utils/FpclService.java
+++ src/main/java/com/rjxx/taxeasy/bizcomm/utils/FpclService.java
@@ -711,6 +711,8 @@ public class FpclService {
         httpPost.addHeader("Content-Type", "text/xml");
         String strMessage = "";
         BufferedReader reader = null;
+        String kplsh=null;
+        key=kplsh;
         StringBuffer buffer = new StringBuffer();
         Map resultMap = null;
         try {
@@ -745,7 +747,6 @@ public class FpclService {
                 }
             }
             int str = key.indexOf("$");
-            String kplsh=null;
             if (str != -1) {
                 kplsh = key.substring(0, str);
             }
