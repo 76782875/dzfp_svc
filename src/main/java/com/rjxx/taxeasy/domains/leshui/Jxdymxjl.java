20522b886c7364af00ef9f6c0b40bcc367d4d5d7
许黎明 <1084854604@qq.com>
2018-02-02 14:00:51 +0800

    no message

--- src/main/java/com/rjxx/taxeasy/domains/leshui/Jxdymxjl.java
+++ src/main/java/com/rjxx/taxeasy/domains/leshui/Jxdymxjl.java
@@ -1,10 +1,14 @@
 package com.rjxx.taxeasy.domains.leshui;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity(name="t_jxdymx_jl")
 public class Jxdymxjl {
-
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String invoicecode;
   private String invoceno;
