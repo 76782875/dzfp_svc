96d39ef0df6c64478a7784119e111264059e05d0
许黎明 <1084854604@qq.com>
2018-02-05 11:57:38 +0800

    no message

--- src/main/java/com/rjxx/utils/CheckOrderUtil.java
+++ src/main/java/com/rjxx/utils/CheckOrderUtil.java
@@ -171,7 +171,7 @@ public class CheckOrderUtil {
             // email
             String Email = (String) jyxxsq.getGfemail();
             if (Email != null && !Email.equals("") && !Email
-                    .matches("^([a-z0-9A-Z]+[-|\\_.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$")) {
+                    .matches("^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$")) {
                 result += ddh + ":购方邮箱(Email)格式有误;";
             }
             // 提取码校验
