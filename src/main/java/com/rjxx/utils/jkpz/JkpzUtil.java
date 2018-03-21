53540f5449d37b5506ea79cfc2305839347b06ea
wyh <243409312@qq.com>
2018-03-21 11:15:40 +0800

    删掉无用代码

--- src/main/java/com/rjxx/utils/jkpz/JkpzUtil.java
+++ src/main/java/com/rjxx/utils/jkpz/JkpzUtil.java
@@ -4,47 +4,11 @@ import com.rjxx.utils.alipay.AlipayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-import java.util.Map;
-
 /**
  * @author: zsq
  * @date: 2018/3/20 17:08
  * @describe: 接口配置类
  */
 public class JkpzUtil {
-
     private  static Logger logger = LoggerFactory.getLogger(AlipayUtils.class);
-
-
-    /**
-     * 通过接口获取用户传入参数值
-     * @param key
-     * @param
-     * @return
-     */
-    public Map param(String key,Object obj){
-        if(key.equals("")){
-
-        }
-
-        return null;
-    }
-
-    public String param2(String key,Object obj){
-        if(key.equals("")){
-
-        }
-
-        return null;
-    }
-
-    public List param3(String key, Object obj){
-        if(key.equals("")){
-
-        }
-
-        return null;
-    }
-
 }
