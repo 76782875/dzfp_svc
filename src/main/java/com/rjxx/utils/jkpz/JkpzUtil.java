7a11e8aba30ed6c29f42258945a638fe5ff177f4
kangzhongxu <kangzhongxu123>
2018-03-22 18:09:04 +0800

    提交商品，优惠信息方法

--- src/main/java/com/rjxx/utils/jkpz/JkpzUtil.java
+++ src/main/java/com/rjxx/utils/jkpz/JkpzUtil.java
@@ -195,50 +195,141 @@ public class JkpzUtil {
     }
 
 
-    public Spvo getSpByid(String gsdm, Integer xfid, Integer skpid, String spdm) {
+    public String getSpByid(Map map) {
         Spvo spvo = new Spvo();
+        Gsxx gsxx = (Gsxx)map.get("gsxx");
+        AdapterPost adapterPost = (AdapterPost)map.get("adapterPost");
+        List<Jymxsq> jymxsqList = (List)map.get("jymxsqList");
+        List<AdapterDataOrderDetails> details =null;
+        Jyxxsq jyxxsq = (Jyxxsq)map.get("jyxxsq");
+        try{
+            details = adapterPost.getData().getOrder().getOrderDetails();
+        }catch (Exception e){
+            return "商品信息缺少金额，税额，价税合计标签";
+        }
         try {
-            Map param = new HashMap();
-            param.put("gsdm", gsdm);
-            param.put("spdm", spdm);
-            spvo = spvoService.findOneSpvo(param);
+            int xh = 1;
+            for(int i=0;i<details.size();i++){
+                AdapterDataOrderDetails ads = details.get(i);
+                Jymxsq jymxsq  = new Jymxsq();
+                Map param = new HashMap();
+                param.put("gsdm", gsxx.getGsdm());
+                param.put("spdm", ads.getVenderOwnCode());
+                spvo = spvoService.findOneSpvo(param);
+                jymxsq.setDdh(jyxxsq.getDdh());
+                jymxsq.setSpmxxh(xh);
+                xh++;
+                jymxsq.setFphxz(ads.getRowType());
+                jymxsq.setSpdm(spvo.getSpbm());
+                jymxsq.setSpmc(spvo.getSpmc());
+                jymxsq.setSpggxh(spvo.getSpggxh());
+                jymxsq.setSpzxbm(spvo.getSpdm());
+                jymxsq.setSpdw(spvo.getSpdw());
+                jymxsq.setSps(ads.getQuantity());
+                jymxsq.setSpdj(ads.getUnitPrice());
+                jymxsq.setKce(ads.getDeductAmount());
+                jymxsq.setSpje(ads.getAmount());
+                jymxsq.setSpse(ads.getTaxAmount());
+                jymxsq.setJshj(ads.getMxTotalAmount());
+                jymxsq.setYkjje(0d);
+                jymxsq.setKkjje(ads.getMxTotalAmount());
+                jymxsq.setYxbz("1");
+                jymxsqList.add(jymxsq);
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            return spvo;
+            return "获取商品信息错误";
         }
-        return spvo;
+        return null;
     }
 
-    public Spvo getSpByMc(String gsdm, Integer xfid, Integer skpid, String spmc) {
+    public String getSpByMc(Map map) {
+        Gsxx gsxx = (Gsxx)map.get("gsxx");
+        AdapterPost adapterPost = (AdapterPost)map.get("adapterPost");
+        List<Jymxsq> jymxsqList = (List)map.get("jymxsqList");
+        List<AdapterDataOrderDetails> details =null;
+        Jyxxsq jyxxsq = (Jyxxsq)map.get("jyxxsq");
         Spvo spvo = new Spvo();
+        try{
+            details = adapterPost.getData().getOrder().getOrderDetails();
+        }catch (Exception e){
+            return "商品信息缺少金额，税额，价税合计标签";
+        }
         try {
-            Map param = new HashMap();
-            param.put("gsdm", gsdm);
-            param.put("spmc", spmc);
-            spvo = spvoService.findOneSpvo(param);
+            int xh = 1;
+            for(int i=0;i<details.size();i++){
+                AdapterDataOrderDetails ads = details.get(i);
+                Jymxsq jymxsq  = new Jymxsq();
+                Map param = new HashMap();
+                param.put("gsdm", gsxx.getGsdm());
+                param.put("spmc", ads.getProductName());
+                spvo = spvoService.findOneSpvo(param);
+
+                jymxsq.setDdh(jyxxsq.getDdh());
+                jymxsq.setSpmxxh(xh);
+                xh++;
+                jymxsq.setFphxz(ads.getRowType());
+                jymxsq.setSpdm(spvo.getSpbm());
+                jymxsq.setSpmc(spvo.getSpmc());
+                jymxsq.setSpggxh(spvo.getSpggxh());
+                jymxsq.setSpzxbm(spvo.getSpdm());
+                jymxsq.setSpdw(spvo.getSpdw());
+                jymxsq.setSps(ads.getQuantity());
+                jymxsq.setSpdj(ads.getUnitPrice());
+                jymxsq.setKce(ads.getDeductAmount());
+                jymxsq.setSpje(ads.getAmount());
+                jymxsq.setSpse(ads.getTaxAmount());
+                jymxsq.setJshj(ads.getMxTotalAmount());
+                jymxsq.setYkjje(0d);
+                jymxsq.setKkjje(ads.getMxTotalAmount());
+                jymxsq.setYxbz("1");
+                jymxsqList.add(jymxsq);
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            return spvo;
+            return "获取商品信息错误";
         }
-        return spvo;
+        return null;
     }
 
-    public Spvo getYhBySp(String spbm, String gsdm) {
+    public String getYhBySp(Map map) {
+        Gsxx gsxx = (Gsxx)map.get("gsxx");
+        List<Jymxsq> jymxsqList = (List)map.get("jymxsqList");
         Spvo spvo = new Spvo();
+
         try {
-            Map param = new HashMap();
-            param.put("gsdm", gsdm);
-            param.put("spbm", spbm);
-            spvo = spvoService.findOneSpvo(param);
+            for(int i=0;i<jymxsqList.size();i++){
+                Jymxsq jymxsq = jymxsqList.get(i);
+                Map param = new HashMap();
+                param.put("gsdm", gsxx.getGsdm());
+                param.put("spbm", jymxsq.getSpdm());
+                spvo = spvoService.findOneSpvo(param);
+                jymxsq.setLslbz(spvo.getLslbz());
+                jymxsq.setYhzcbs(spvo.getYhzcbs());
+                jymxsq.setYhzcmc(spvo.getYhzcmc());
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            return spvo;
+            return "商品优惠信息处理有误！";
         }
-        return spvo;
+        return null;
     }
 
-    public Map getYhByBmsl(String spbm, Double sl) {
-        return getLsvBz.getLsvBz(sl, spbm);
+    public String getYhByBmsl(Map map) {
+        List<Jymxsq> jymxsqList = (List)map.get("jymxsqList");
+        try {
+            for(int i=0;i<jymxsqList.size();i++){
+                Jymxsq jymxsq = jymxsqList.get(i);
+                Map result = getLsvBz.getLsvBz(jymxsq.getSpsl(), jymxsq.getSpdm());
+                jymxsq.setLslbz(String.valueOf(result.get("lslbz")));
+                jymxsq.setYhzcbs(String.valueOf(result.get("yhzcbs")));
+                jymxsq.setYhzcmc(String.valueOf(result.get("yhzcmc")));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "商品优惠信息处理有误！";
+        }
+        return null;
     }
 
 }
