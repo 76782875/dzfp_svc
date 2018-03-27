9ad1e4e6d918e900acc4ccdb9a89c1ad99356655
zhangsongqiang <zhangsongqiang@datarj.com>
2018-03-27 10:51:15 +0800

    添加接口模板表

---
+++ src/main/java/com/rjxx/taxeasy/dao/JkmbbJpaDao.java
@@ -0,0 +1,17 @@
+package com.rjxx.taxeasy.dao;
+
+import com.rjxx.taxeasy.domains.Jkmbb;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * 由GenJavaCode类自动生成
+ * <p>
+ * Tue Mar 27 10:47:00 CST 2018
+ *
+ * @ZhangBing
+ */
+
+public interface JkmbbJpaDao extends CrudRepository<Jkmbb, Integer> {
+
+}
+
