9d421a55a6195db93c82ace50a88e616c9fb05fa
zhangsongqiang <zhangsongqiang@datarj.com>
2018-03-27 13:59:21 +0800

    update

--- src/main/java/com/rjxx/taxeasy/dao/JkmbzbJpaDao.java
+++ src/main/java/com/rjxx/taxeasy/dao/JkmbzbJpaDao.java
@@ -1,7 +1,10 @@
 package com.rjxx.taxeasy.dao;
 
 import com.rjxx.taxeasy.domains.Jkmbzb;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * 由GenJavaCode类自动生成
@@ -12,5 +15,7 @@ import org.springframework.data.repository.CrudRepository;
  */ 
 public interface JkmbzbJpaDao extends CrudRepository<Jkmbzb, Integer> {
 
+    @Query(nativeQuery = true,value = "select * from t_jkmbzb where mbid=?1 ")
+    List<Jkmbzb> findListByMbid(Integer mbid);
 }
 
