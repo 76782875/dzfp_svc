d789fca41dd98c207acbb8090770183ab27a8ac8
kangzhongxu <kangzhongxu@datarj.com>
2018-04-09 17:56:41 +0800

    skp新增构造方法

--- src/main/java/com/rjxx/taxeasy/dao/PpJpaDao.java
+++ src/main/java/com/rjxx/taxeasy/dao/PpJpaDao.java
@@ -4,6 +4,8 @@ import com.rjxx.taxeasy.domains.Pp;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 /**
  * 由GenJavaCode类自动生成
  * <p>
@@ -14,5 +16,8 @@ import org.springframework.data.repository.CrudRepository;
 public interface PpJpaDao extends CrudRepository<Pp, Integer> {
     @Query(nativeQuery = true, value = "select * from t_pp where id=?1 and yxbz='1'")
     Pp findOneById(Integer id);
+
+    @Query(nativeQuery = true, value = "select * from t_pp where ppdm=?1 and gsdm=?2 and yxbz='1'")
+    List<Pp> findAllByPpdm(String ppdm, String gsdm);
 }
 
