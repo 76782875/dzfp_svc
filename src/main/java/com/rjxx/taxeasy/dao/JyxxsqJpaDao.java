22ccef0a9faeff32dbab4ef679a9b696d282aa59
zhangsongqiang <zhangsongqiang@datarj.com>
2018-05-08 16:59:42 +0800

    修改拉取数据方法

--- src/main/java/com/rjxx/taxeasy/dao/JyxxsqJpaDao.java
+++ src/main/java/com/rjxx/taxeasy/dao/JyxxsqJpaDao.java
@@ -27,5 +27,12 @@ public interface JyxxsqJpaDao extends CrudRepository<Jyxxsq, Integer> {
 
     @Query(nativeQuery = true,value = "select * from t_jyxxsq where sqlsh=?1 and gsdm=?2 and yxbz='1' ")
     Jyxxsq findOneBySqlshAndGsdm(String sqlsh, String gsdm);
+
+    @Query(nativeQuery = true,value = "select * from t_jyxxsq where gsdm=?1 and tqm=?2 and yxbz='1' ")
+    Jyxxsq findOneByGsdmAndTqm(String gsdm, String tqm);
+
+
+    @Query(nativeQuery = true,value = "select * from t_jyxxsq where gsdm=?1 and ddh=?2 and ztbz in('5','6','3') and yxbz='1' ")
+    Jyxxsq findOneByGsdmAndDdh(String gsdm, String ddh);
 }
 
