e2e18fc88ac5fa23cf3ae26618b0a235f175a4af
zhangsongqiang <zhangsongqiang@datarj.com>
2018-05-10 14:12:31 +0800

    修改查询语句

--- src/main/java/com/rjxx/taxeasy/dao/leshui/JxywjlJpaDao.java
+++ src/main/java/com/rjxx/taxeasy/dao/leshui/JxywjlJpaDao.java
@@ -10,6 +10,6 @@ import java.util.List;
  * Created by wangyahui on 2018/2/1 0001.
  */
 public interface JxywjlJpaDao extends JpaRepository<Jxywjl,Integer> {
-    @Query(nativeQuery = true,value = "select * from t_jxyw_jl where gfid=?1 and gsdm=?2 order by lrsj desc limit 1")
+    @Query(nativeQuery = true,value = "select * from t_jxyw_jl where gfid=?1 and gsdm=?2 and kssj is not null and jssj is not null order by lrsj desc limit 1")
     Jxywjl findOneByGfId(Integer gfid, String gsdm);
 }
