ba68bba694a948a2b5bdb3985d9bd548473cae1a
赵超超 <869404182@qq.com>
2018-05-10 18:53:39 +0800

    修改开票流水查询，关联t_ztbz,开票流水处理添加无效功能

---
+++ src/main/java/com/rjxx/taxeasy/vo/Yjfsvo.java
@@ -0,0 +1,59 @@
+package com.rjxx.taxeasy.vo;
+
+import java.io.Serializable;
+
+public class Yjfsvo implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String jshj;
+	private String jylssj;
+    private String ddh;
+    private String gfmc;
+    private String gfemail;
+    private String serialorder;
+    private String sfkp;
+	public String getJshj() {
+		return jshj;
+	}
+	public void setJshj(String jshj) {
+		this.jshj = jshj;
+	}
+	public String getJylssj() {
+		return jylssj;
+	}
+	public void setJylssj(String jylssj) {
+		this.jylssj = jylssj;
+	}
+	public String getDdh() {
+		return ddh;
+	}
+	public void setDdh(String ddh) {
+		this.ddh = ddh;
+	}
+	public String getGfmc() {
+		return gfmc;
+	}
+	public void setGfmc(String gfmc) {
+		this.gfmc = gfmc;
+	}
+	public String getGfemail() {
+		return gfemail;
+	}
+	public void setGfemail(String gfemail) {
+		this.gfemail = gfemail;
+	}
+	public String getSerialorder() {
+		return serialorder;
+	}
+	public void setSerialorder(String serialorder) {
+		this.serialorder = serialorder;
+	}
+	public String getSfkp() {
+		return sfkp;
+	}
+	public void setSfkp(String sfkp) {
+		this.sfkp = sfkp;
+	}
+    
+    
+}
