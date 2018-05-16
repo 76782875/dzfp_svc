c6ce8fd3fb554376231bb020d1b1a07381c74c4a
zhangsongqiang <zhangsongqiang@datarj.com>
2018-05-16 19:08:41 +0800

    修改

--- src/main/java/com/rjxx/taxeasy/bizcomm/utils/UploadFpckxxService.java
+++ src/main/java/com/rjxx/taxeasy/bizcomm/utils/UploadFpckxxService.java
@@ -1,6 +1,5 @@
 package com.rjxx.taxeasy.bizcomm.utils;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -18,7 +17,7 @@ import com.rjxx.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
