a65e619905227e374ee2dd01b20b0a449373f5d7
wyh <243409312@qq.com>
2018-05-28 17:47:40 +0800

    1.1

--- src/main/java/com/rjxx/utils/alipay/AlipayRSAUtil.java
+++ src/main/java/com/rjxx/utils/alipay/AlipayRSAUtil.java
@@ -66,8 +66,8 @@ public class AlipayRSAUtil {
         Signature signature = Signature.getInstance("SHA1withRSA");
         signature.initVerify(publicK);
         signature.update(srcData.getBytes("GBK"));
-//        return signature.verify(Base64.decodeBase64(sign.getBytes()));
-        return signature.verify(sign.getBytes());
+        return signature.verify(Base64.decodeBase64(sign.getBytes()));
+//        return signature.verify(sign.getBytes());
     }
 
     /**
