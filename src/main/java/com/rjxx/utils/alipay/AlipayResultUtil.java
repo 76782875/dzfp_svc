c5d2e2edac1794f165f0927b730d66cf06d71066
wyh <243409312@qq.com>
2018-05-29 16:20:33 +0800

    修改alipayUtil，增加bean一个字段applyId

--- src/main/java/com/rjxx/utils/alipay/AlipayResultUtil.java
+++ src/main/java/com/rjxx/utils/alipay/AlipayResultUtil.java
@@ -2,7 +2,6 @@ package com.rjxx.utils.alipay;
 
 import com.rjxx.taxeasy.dto.alipay.AlipayResult;
 
-import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,26 +12,14 @@ import java.util.Map;
  * @date 2018/5/15
  */
 public class AlipayResultUtil {
-//    public static AlipayResult result(String code,String msg,String fpdm,String fphm){
-//        AlipayResult result = new AlipayResult();
-//        result.setResultCode(code);
-//        result.setResultMsg(msg);
-//        result.setEinvCode(fpdm);
-//        result.setEinvNo(fphm);
-//        return result;
-//    }
-
     public static AlipayResult result(String code,String msg){
         Map map = new HashMap();
         map.put("resultCode", code);
         map.put("resultMsg", msg);
-        //获取原始加签内容
-        String signatureContent = AlipaySignUtil.getSignatureContent(map);
 
         String sign = "";
-        //获取签名,得到签名之后进行urlencode
         try {
-            sign = URLEncoder.encode(AlipaySignUtil.sign(signatureContent, AlipaySignUtil.getPrivateKey(AlipaySignUtil.PRIKEY)), "utf-8");
+            sign= AlipaySignUtil.getSign(map,AlipaySignUtil.getPrivateKey(AlipaySignUtil.PRIKEY));
         } catch (Exception e) {
             e.printStackTrace();
         }
