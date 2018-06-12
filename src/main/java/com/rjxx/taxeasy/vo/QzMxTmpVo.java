122e18906fb7c18eb94bbd4ba559262be43c4e43
kangzhongxu <kangzhongxu@datarj.com>
2018-06-12 13:41:22 +0800

    处理全部折扣的分票方法。

---
+++ src/main/java/com/rjxx/taxeasy/vo/QzMxTmpVo.java
@@ -0,0 +1,53 @@
+package com.rjxx.taxeasy.vo;
+
+
+import java.math.BigDecimal;
+
+public class QzMxTmpVo {
+
+	private Integer fpnum;
+	private Integer mxcount;
+	private BigDecimal spje;
+	private BigDecimal jshj;
+	private String fphxz;
+
+	public Integer getFpnum() {
+		return fpnum;
+	}
+
+	public void setFpnum(Integer fpnum) {
+		this.fpnum = fpnum;
+	}
+
+	public Integer getMxcount() {
+		return mxcount;
+	}
+
+	public void setMxcount(Integer mxcount) {
+		this.mxcount = mxcount;
+	}
+
+	public BigDecimal getSpje() {
+		return spje;
+	}
+
+	public void setSpje(BigDecimal spje) {
+		this.spje = spje;
+	}
+
+	public BigDecimal getJshj() {
+		return jshj;
+	}
+
+	public void setJshj(BigDecimal jshj) {
+		this.jshj = jshj;
+	}
+
+	public String getFphxz() {
+		return fphxz;
+	}
+
+	public void setFphxz(String fphxz) {
+		this.fphxz = fphxz;
+	}
+}
